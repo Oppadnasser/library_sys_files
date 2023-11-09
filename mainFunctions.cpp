8723a0a610ccf6b0121367c9d2e9724d34392c2c
adel-3 <aboadel1512003@gmail.com>
2023-11-09 14:19:32 +0200

    add the main class

---
+++ mainFunctions.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+void addNewAuthor() {}
+
+void addNewBook() {}
+
+void updateAuthorName(int authorId) {}
+
+void updateBookTitle(int ISBN) {}
+
+void deleteBook(int ISBN) {}
+
+void deleteAuthor(int ID) {}
+
+void printAuthor(int ID) {}
+
+void printBook(int ISBN) {}
+
+void writeQuery() {}
+
+void Exit() {
+    cout << "GOOD BYE MY DEAR USER :)";
+    exit(1);
+}
